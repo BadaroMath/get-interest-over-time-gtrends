fb47cc4971cdfa3d331b0a2a803cf69d62549e15
José Matheus Badaró <badaromath@gmail.com>
2023-03-02 15:08:18 -0300

    Generalization commit
    Refactored code to increase generalization and modularity. Changed hard-coded values to dynamic variables passed as parameters, allowing for greater flexibility in data extraction, transformation, and loading. Renamed functions and variables to better reflect their general purpose and increase readability. Cleaned up code formatting for consistency and maintainability.

--- main.py
+++ main.py
@@ -1,30 +1,78 @@
 """
 Getting interest over time daily from google trends.
+author: jose.badaro@raccoon.ag
 
-author: BadaroMath
+Script that collects and transforms data from Google Trends and saves it to BigQuery tables. 
+It includes functions for configuring the logging settings, getting the payload from a 
+cloud scheduler, collecting daily and monthly data from Google Trends using the pytrends 
+package, and saving the data to BigQuery using the google-cloud-bigquery package.
+
+Payload example:
+
+{
+    "message": {
+        "config": {
+        "project_id": "<project_id>",
+        "dataset_id": "<dataset_id>",
+        "table_name": "<table_name>",
+        "keyword": "<keyword>",
+        "since": "<since_date>"
+        }
+    }
+}
 """
 import pandas as pd
 from datetime import date, datetime
 from pytrends.request import TrendReq
 import logging
-import csv
 from dateutil.relativedelta import relativedelta
+import logging as log
+from google.cloud import bigquery
 
-def save_csv(daily, monthly, path):
+
+def config_log():
+
+    """
+    Configure logging level, output format and sends it to a file.
+    """
+
+    today = datetime.today().strftime("%Y%m%d")
+    logging_level = 20
+    # log_filename = f'logs/instagram_organic_{today}.log'
+    log.basicConfig(
+        # filename=log_filename,
+        level=logging_level,
+        format=f'[%(asctime)s.%(msecs)03d] %(levelname)s: %(funcName)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        force=True)
     
-    logging.info(f'Transform and save data into {path}')
-    df = pd.merge(daily, monthly, how="left", on=["year", "month"])
-    df = df.drop(["month", "year"], axis=1)
-    df["outback_aj"] = df["outback"]*df["outback_monthly"]/100
-    df.to_csv(
-        path,
-        #mode = 'a',
-        header = True,
-        index = False
-        )
-    return f'Saved'
 
-def get_hits(time_ranges):
+
+def payload(request_json):
+    """
+    Retrieves the payload from the cloud scheduler and returns the configuration 
+    needed for saving the data to BigQuery.
+
+    Arguments:
+        request {payload body} -- payload body from cloud scheduler.
+
+    Returns:
+        config {dict} -- transformed report result
+    """    
+    if request_json and 'message' in request_json:
+        config = request_json['message']["config"]
+
+        return config
+    else:
+        return ("Payload error", 400)
+
+def get_hits(time_ranges, config):
+    '''
+    Collects daily interest over time from Google Trends for a given time range and 
+    returns a pandas dataframe with the results. It uses the pytrends package to build 
+    a payload with the specified keyword and time range and collects the interest over time data. 
+    If there is an error, it retries up to 5 times.
+    '''
     logging.info('Collecting interest over time:')
     pytrend = TrendReq()
     trends = pd.DataFrame()     
@@ -35,7 +83,7 @@ def get_hits(time_ranges):
         if datetime.strptime(timerange.split()[0], "%Y-%m-%d") <= datetime.today():    
             while attempts <= 5:
                 try:
-                    pytrend.build_payload(kw_list=['outback'],timeframe=timerange, geo='BR')
+                    pytrend.build_payload(kw_list=[config["keyword"]],timeframe=timerange, geo='BR')
                     hits = pytrend.interest_over_time()                    
                     trends = pd.concat([trends, hits])
                     break
@@ -53,14 +101,20 @@ def get_hits(time_ranges):
         trends["year"] = pd.to_datetime(trends["date"], format = "%Y-%m-%d").dt.year
     return trends
 
-def get_monthly():
+def get_monthly(config):
+    '''
+    Collects monthly interest over time from Google Trends for all time and returns a 
+    pandas dataframe with the results. It uses the pytrends package to build a payload 
+    with the specified keyword and collects the interest over time data. If there is an 
+    error, it retries up to 5 times.
+    '''
     logging.info('Collecting interest over time all time:')
     pytrend = TrendReq()
     hits_all = None
     t=0
     while t <= 5:
         try:
-            pytrend.build_payload(kw_list=['outback'],timeframe='all', geo='BR')
+            pytrend.build_payload(kw_list=[config["keyword"]],timeframe='all', geo='BR')
             hits_all = pytrend.interest_over_time()
             break
         except:
@@ -72,17 +126,91 @@ def get_monthly():
         hits_all.reset_index(inplace = True)
         hits_all = hits_all.drop("isPartial", axis=1)
         hits_all = hits_all[(hits_all['date'] >= '2021-01-01')]
-        hits_all = hits_all.rename(columns={'outback': 'outback_monthly'})
+        hits_all = hits_all.rename(columns={f'{config["keyword"]}': f'{config["keyword"]}_monthly'})
         hits_all["month"] = pd.to_datetime(hits_all["date"], format = "%Y-%m-%d").dt.month
         hits_all["year"] = pd.to_datetime(hits_all["date"], format = "%Y-%m-%d").dt.year
         hits_all = hits_all.drop("date", axis=1)
         
     return hits_all 
 
-def main():
+
+def bigquery_save_data(data, config):
+    """
+    Function saves the data to a BigQuery table. 
+    It uses the google-cloud-bigquery package to load the dataframe 
+    to the specified table in BigQuery.
+
+    Arguments:
+        config {dict} -- configuration with BigQuery params
+        data {df} -- df data to be loaded
+    """
+
+    log.info(f"Loading data to {config['table_name']}")
+
+    bigquery_client = bigquery.Client(config["project_id"])
+    dataset = bigquery_client.dataset(config["dataset_id"])
+    table_ref = dataset.table(config["table_name"])
+
+    job_config = bigquery.LoadJobConfig()
+    job_config.create_disposition = "CREATE_IF_NEEDED"
+    #job_config.time_partitioning = bigquery.table.TimePartitioning()
+    #job_config.schema = schema
+    job_config.autodetect = True
+    job_config.write_disposition = "WRITE_TRUNCATE"
+
+    job = bigquery_client.load_table_from_dataframe(
+        data,
+        table_ref,
+        job_config=job_config
+    )
+
+    try:
+        job.result()
+    except Exception:
+        log.error(job.errors)
+
+
+
+def save_data(daily, monthly, config):
+    """
+    function saves the daily and monthly data to BigQuery tables. 
+    It first merges the daily and monthly dataframes and then saves the merged 
+    dataframe to a BigQuery table using the bigquery_save_data() function.
+
+    Arguments:
+        daily {df} -- daily data from grends
+        monthly {df} -- monthly data from gtrends
+        config {dict} -- configuration with BigQuery params
+    """    
+    logging.info(f"Transform and save data into {config['table_name']}")
+    kw = config['keyword']
+    df = pd.merge(daily, monthly, how="left", on=["year", "month"])
+    df = df.drop(["month", "year"], axis=1)
+    df[f"{kw}_daily"] = df[kw]*df[f"{kw}_monthly"]/100
+    df = df.drop([kw], axis=1)
+    print(df)
+    bigquery_save_data(df, config)
+
+    return f'Saved'
+
+
+def etl(config):
+    '''
+    Performs the Extract, Transform and Load (ETL) process on the data 
+    based on the input configuration config.
     
+    The function first generates a list of monthly periods from config["since"] 
+    until the current month using the pd.date_range() function from the pandas library. 
+    It then iterates through this list and transforms each period to a string representing 
+    the first and last day of that month.
+
+    Next, the function calls the get_hits() and get_monthly() functions to extract data from 
+    an external source and transform it into pandas DataFrames. These DataFrames are then passed
+    to the save_data() function to be loaded into a database or file, as specified in the config 
+    parameter.
+    '''
     PERIODOS = pd.date_range(
-        '2020-01-01',
+        config["since"],
         date.today().strftime(
             "%Y-%m-%d"
         ),
@@ -103,19 +231,19 @@ def main():
         )
         PERIODOS[i] = first_date + " " + last_day
         
-    daily = get_hits(PERIODOS)
-    monthly = get_monthly()
-    print(daily, monthly)
-    save_csv(daily,monthly,r'D:\Tabelas\gtrends.csv')
+    daily = get_hits(PERIODOS, config)
+    monthly = get_monthly(config)
+    #print(daily.head(3), monthly.head(3))
+    save_data(daily, monthly, config)
+
     return ("ok", 200)
 
-if __name__=="__main__":
-    logging.basicConfig(
-        filename=r'D:/Rotinas/Gtrends_Log.log',
-        format='%(levelname)s %(asctime)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-        filemode="a",
-        level=logging.INFO
-    )
-    main()
-   
+
+def main(request):
+    request_json = request.get_json()
+    config = payload(request_json)
+    status_code = etl(config)
+
+    if status_code == 500:
+        return ("not ok", 500)
+    return ("ok", 200)
